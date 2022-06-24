e67b8a937f28568c0191df13f7f01a19e9200d36
MizaGBF <47531574+MizaGBF@users.noreply.github.com>
2022-06-24 10:52:10 +0200

    3.4.14 forgot version bump

--- WiihUb.py
+++ WiihUb.py
@@ -107,7 +107,7 @@ class Handler(BaseHTTPRequestHandler):
 
 class WiihUb(ThreadingHTTPServer):
     def __init__(self):
-        self.version = "v3.4.13"
+        self.version = "v3.4.14"
         print("Starting...\n")
         try:
             with open('config.json', 'rb') as f:
